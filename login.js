faaa392048c72e1f90ee217594f1c407c04fab06
LaurysP <laurysportorreal.com>
2025-01-18 18:56:35 -0400

    nuevo

---
+++ login.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const wrapper = document.querySelector('.wrapper');
+    const loginLink = document.querySelector('.login-link');
+    const registerLink = document.querySelector('.register-link');
+    const accesoBtn = document.querySelector('.btn.animation');
+
+    // Maneja el cambio entre los formularios
+    registerLink.addEventListener('click', () => {
+        wrapper.classList.add('active');
+    });
+
+    loginLink.addEventListener('click', () => {
+        wrapper.classList.remove('active');
+    });
+
+    // Redirige a otra página cuando se hace clic en "Acceso"
+    accesoBtn.addEventListener('click', () => {
+        window.location.href = './Interfaz/index.html'; // Cambia 'otra-pagina.html' por la URL deseada
+    });
+});
